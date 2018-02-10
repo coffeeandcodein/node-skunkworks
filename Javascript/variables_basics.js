d01a5de09b0b35ec9680222d9a4f059a3e796b33
tenthdoctor <doctorandabox@gmail.com>
2018-02-10 11:27:10 +0530

    javascript basic functions

---
+++ Javascript/variables_basics.js
@@ -0,0 +1,26 @@
+var x = 3,
+    y = 2.5;
+
+var z = '3';
+var result = x > y;
+
+var writeGreeting = function(name) {
+  var message = "Hello, " + name;
+  console.log(message);
+};
+
+writeGreeting("doctor");
+writeGreeting("tardis");
+
+var answer = parseInt(z) + y;
+console.log(answer);
+console.log(typeof answer);
+
+console.log(typeof result);
+console.log(typeof y);
+console.log(typeof z);
+console.log(typeof console);
+console.log(typeof console.log);
+
+x = 3;
+console.log(x);
