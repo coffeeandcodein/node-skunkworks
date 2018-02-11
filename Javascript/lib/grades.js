5c94d046f94dc97bf42af44f6e9a62eb915ebd54
tenthdoctor <doctorandabox@gmail.com>
2018-02-12 01:00:00 +0530

    modules

---
+++ Javascript/lib/grades.js
@@ -0,0 +1,26 @@
+var gradeBook = {
+
+  _grades: [], //Generally underscore is used if we want it to be truly local variable
+  addGrade: function(newGrade) {
+    this._grades.push(newGrade);
+  },
+
+  getCountOfGrades: function() {
+    return this._grades.length;
+  },
+
+  getAverage: function() {
+    var total = 0;
+    for (var i = 0; i < this._grades.length; i++) {
+      total += this._grades[i];
+    }
+    return total/this._grades.length;
+  },
+
+  reset: function() {
+    this._grades = [];
+  }
+
+};
+
+exports.book = gradeBook;
